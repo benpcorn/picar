9f973016966aba836b367a0d1a9504d5eaade925
Benjamin Corn <benjcorn@amazon.com>
2022-02-10 19:40:13 -0500

    Scan list

--- Part2/navigate-02.py
+++ Part2/navigate-02.py
@@ -52,56 +52,11 @@ def get_distance_at(angle):
     angle_distance = [angle, distance]
     return int(distance), int(angle)
 
-def scan_step(ref):
-    global scan_list, current_angle, us_step
-    current_angle += us_step
-    if current_angle >= max_angle:
-        current_angle = max_angle
-        us_step = -STEP
-    elif current_angle <= min_angle:
-        current_angle = min_angle
-        us_step = STEP
-
-    status = get_distance_at(current_angle)
-    scan_list.append(status)
-
-    if current_angle == min_angle or current_angle == max_angle:
-        if us_step < 0:
-            scan_list.reverse()
-        tmp = scan_list.copy()
-        scan_list = []
-        return tmp
-    else:
-        return False
-
 def scan():
-    while True:
-        scan_detection_list = scan_step(30)
-        if not scan_detection_list or len(scan_detection_list) < NUM_SAMPLES - 1:
-            continue
-        break
+    for i in range (min_angle, max_angle, 5):
+        scan_list.append(get_distance_at(i))
+        time.sleep(0.2)
 
-    return scan_detection_list
 
-def draw_map(detection_list, size):
-    env_map = np.zeros([size, size])
-    mask = np.ones([10, 7])
-    point_y, point_x = detection_list[0]
-    for sample in detection_list:
-        distance, angle = sample
-        if distance > 0:
-            x = math.floor((math.sin(math.radians(-angle))) * distance) + 50
-            y = math.floor((math.cos(math.radians(angle))) * distance)
-            if 100 > x > 0 and y < 100 and (abs(point_y - y) <= 1) \
-                    and (abs(point_x - x) <= 1):
-                env_map[x, y] = 1        
-            point_x = x
-            point_y = y
-            
-    env_map = binary_dilation(env_map, mask).astype(env_map.dtype)
-    return np.rot90(env_map)
-
-origin = (99, 50)
-target = (0, 50)
-detection_list = scan()
-env_map = draw_map(detection_list, 100)
+scan()
+print(scan_list)
