ba2ff081748cad7733d8f8c9c1202ab7365bd730
Benjamin Corn <benjcorn@amazon.com>
2022-02-12 18:23:56 -0500

    New

--- Part3/stop.py
+++ Part3/stop.py
@@ -2,6 +2,10 @@ from Motor import *
 
 motor = Motor()
 
+# Forward
+motor.setMotorModel(-1000,-1000,-1000,-1000) 
+time.sleep(2)
+
 # Turn Right
 motor.setMotorModel(-2000,-2000,1500,1500) 
 time.sleep(1)
@@ -10,5 +14,5 @@ time.sleep(1)
 
 # Turn Left
 motor.setMotorModel(1500,1500,-2000,-2000) 
-time.sleep(1)
+time.sleep(1.2)
 motor.setMotorModel(0,0,0,0)
