88d8d62eeea0a9d4daf4eb85f50ec3d18a26c36d
Benjamin Corn <benjcorn@amazon.com>
2022-02-12 12:36:28 -0500

    New

--- Part3/mapping.py
+++ Part3/mapping.py
@@ -420,7 +420,7 @@ class Location:
         """
         :return: speed in cm/s
         """
-        speed_reading = 1
+        speed_reading = .2
         # print(f"Current speed: {speed_reading} cm/s")
         return speed_reading
 
